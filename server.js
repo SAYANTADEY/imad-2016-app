f221841a4064feeb296c32befdf59b4bdad6c31b
SAYANTADEY <sayantadey0@gmail.com>
2016-09-27 19:34:39 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ var path = require('path');
 
 var app = express();
 app.use(morgan('combined'));
-var articles = {
+var articles : {
     var 'article-one' : {
         title: 'Article One I Sayanta Dey',
         heading: 'Article One',
