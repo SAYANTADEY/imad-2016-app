d8d37f92d5b888ace54307cdc88a54d18c531091
SAYANTADEY <sayantadey0@gmail.com>
2016-09-27 19:31:17 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -10,13 +10,7 @@ var articles = {
         heading: 'Article One',
         date: 'Sep 5, 2016',
         content: `<p>
-                        This is the content for my first article. This is the content for my first article. This is the content for my first article. This is the content for my first article. This is the content for my first article. This is the content for my first article.
-                    </p>    
-                    <p>
-                        This is the content for my first article. This is the content for my first article. This is the content for my first article. This is the content for my first article. This is the content for my first article. This is the content for my first article.
-                    </p>
-                    <p>
-                        This is the content for my first article. This is the content for my first article. This is the content for my first article. This is the content for my first article. This is the content for my first article. This is the content for my first article.
+                        This is the content for my first article.
                     </p>`
     },
     var 'article-two' : {
