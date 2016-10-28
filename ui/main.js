d89384bcef02170b8d8e63d70b61da8b315c8324
SAYANTADEY <sayantadey0@gmail.com>
2016-10-28 23:20:59 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -5,6 +5,11 @@ var element = document.getElementById('main-text');
 element.innerHTML = 'New value';
 
 var img = document.getElementById('madi');
+var marginLeft = 0;
+function moveRight () {
+    marginLeft = marginRight + 10;
+    img.style.marginLeft = marginLeft + 'px';
+}
 img.onclick = function () {
-    img.style.marginleft = '100px';
+    var interval = setInterval(moveRight, 100);
 };
