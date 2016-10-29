a8b8a9022946ddcd7252e2578f25f9fb55367656
SAYANTADEY <sayantadey0@gmail.com>
2016-10-29 16:08:26 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -18,5 +18,6 @@ var nameInput = document.getElementById('name');
 var name = nameInput.value;
 var submit =  document.getElementById('submit_btn');
 submit.onclick = function () {
-    
+    var names = ['name1', 'name2', 'name3'];
+    var list = '';
 };
