8d5bd9be73d1450f1617b9ec2d1ae934caa0811a
SAYANTADEY <sayantadey0@gmail.com>
2016-11-02 06:50:17 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -1,1 +1,19 @@
+var express = require('express');
+var morgan = require('morgan');
+var path = require('path');
 
+var app = express();
+app.use(morgan('combined'));
+
+app.get('/', function (req, res) {
+    res.sendFile(path.join(__dirname, 'ui', 'index.html'));
+});
+
+app.get('/ui/style.css', function (req, res) {
+    res.sendFile(path.join(__dirname, 'ui', 'style.css'));
+});
+
+var port = 8080;
+app.listen(8080, function () {
+    console.log('IMAD course app listening on port ${port}!');
+});
