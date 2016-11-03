8e4cea8923098d403ce719816c725a63e048f836
SAYANTADEY <sayantadey0@gmail.com>
2016-11-03 21:25:47 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -54,7 +54,7 @@ app.get('/', function (req, res) {
     res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
-app.get('/', function (req, res) {
+app.get('/about-me', function (req, res) {
     res.sendFile(path.join(__dirname, 'ui', 'about-me.html'));
 });
 
