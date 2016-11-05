04e459a7f8df8e6bb09e23dddbaad591389d8a18
SAYANTADEY <sayantadey0@gmail.com>
2016-11-05 09:54:39 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -14,41 +14,6 @@ var config = {
 var app = express();
 app.use(morgan('combined'));
 
-function createTemplate (data) {
-    var title = data.title;
-    var date = data.date;
-    var author = data.author;
-    var content = data.content;
-    var htmlTemplate = `
-    <html>
-      <head>
-          <title>    
-             ${title}
-          </title>
-          <meta name="viewport" content="width=device-width, initial-scale=1" />
-          <link href="/ui/style.css" rel="stylesheet" />
-      </head>
-      <body>
-          <div class="container">
-              <div>
-                  <a href="/">Home</a>
-              </div>
-              <div>
-                  ${date.toDateString()}
-              </div>
-              <div>
-                 ${author}
-              </div>
-              <div>
-                 ${content}
-              </div>
-          </div>
-      </body>  
-    </html>
-    `;
-    return htmlTemplate;
-}
-
 app.get('/', function (req, res) {
     res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
@@ -66,43 +31,6 @@ app.get('/blog', function (req, res) {
 });
 
 var pool = new Pool(config);
-app.get('/test-db', function (req, res) {
-    pool.query('SELECT * FROM test', function(err,result) {
-       if(err) {
-           res.status(500).send(err.toString());
-       } else {
-           res.send(JSON.stringify(result.rows));
-       }
-    });
-});
-
-var counter = 0;
-app.get('/counter', function (req, res) {
-    counter = counter + 1;
-    res.send(counter.toString());
-});
-
-var names = [];
-app.get('/submit-name', function (req, res) {
-    var name = req.query.name;
-    names.push(name);
-    res.send(JSON.stringify(names));
-});
-
-app.get('/blog/:articleName', function (req, res) {
-    pool.query("SELECT * FROM blog_post WHERE author = $1", [req.params.articleName], function (err, result) {
-        if(err) {
-           res.status(500).send(err.toString());
-       } else {
-           if (result.rows.length === 0) {
-               res.status(404).send('Article not found');
-           } else {
-               var articleData = result.rows[0];
-               res.send(createTemplate(articleData));
-           }
-       }
-    });
-});
 
 app.get('/ui/style.css', function (req, res) {
     res.sendFile(path.join(__dirname, 'ui', 'style.css'));
